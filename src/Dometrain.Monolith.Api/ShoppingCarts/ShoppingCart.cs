501fc14348a85c48b9723c9d0e901648e0afd503
Nick Chapsas <nick.chapsas@gmail.com>
2025-10-24 13:50:30 +0100

    pre ex

--- src/Dometrain.Monolith.Api/ShoppingCarts/ShoppingCart.cs
+++ src/Dometrain.Monolith.Api/ShoppingCarts/ShoppingCart.cs
@@ -1,9 +1,14 @@
+using Newtonsoft.Json;
+
 namespace Dometrain.Monolith.Api.ShoppingCarts;
 
 public class ShoppingCart
 {
+    [JsonProperty("pk")]
+    [System.Text.Json.Serialization.JsonIgnore]
     public string Pk => StudentId.ToString();
     
+    [JsonProperty("id")]
     public required Guid StudentId { get; set; }
 
     public List<Guid> CourseIds { get; set; } = [];
